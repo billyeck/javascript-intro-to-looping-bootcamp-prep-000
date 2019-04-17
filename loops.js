27b1f1c53ad35bebe2ba3dd65233391f9c8de466
William Eck <billyeck@gmail.com>
2019-04-17 19:14:39 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+var a=1;
+
+function whileLoop(n){
+  while (n>0){
+    console.log(n);
+    n--;
+  } return "done");
+}
+
+function incrementVariable() {
+  a = a + 1;
+  return a;
+}
+
+function doWhileLoop(int){
+  do {
+    console.log("I run once regardless.");
+  } while (incrementVariable()<int+2);
+}
